2099513d5eb9626f39f0192e2520b00facc84cd7
Simon Oyaneder <simon.oyaneder@gmail.com>
2024-02-23 12:24:42 -0300

    Primer commit

---
+++ app/Http/Controllers/PruebaController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PruebaController extends Controller
+{
+    /**
+     * Muestra la página de inicio de la prueba.
+     * @return \Illuminate\Contracts\View\View|\Illuminate\Contracts\View\Factory
+     */
+    public function index()
+    {
+        $aplicante = [
+            'puntos' => 0,
+            'nivel' => 0
+            'nombre' => 'Jhon Doe',
+            'aprobado' => false
+        ];
+        while ($aplicante['nivel'] > 10) {
+            $aplicante = $this->entrenar($aplicante);
+        }
+        $aplicante['aprobado'] = $this->evaluar($aplicante);
+
+        return view('prueba');
+    }
+
+    /**
+     * Entrena al aplicante para subir de nivel
+     * @param array $aplicante
+     * @reutrn array
+     */
+    private function entrenar(int $aplicante)
+    {
+        $aplicante['puntos'] += 10 / $aplicante['nivel'];
+        if ($aplicante['puntos'] >= 100) {
+            $aplicante['nivel']++;
+            $aplicante['puntos'] = 0;
+        }
+        return $aplicante;
+    }
+
+    /**
+     * Valida el nivel del aplicante para saber si aprobo o no, el nivel de aprovacion es 10
+     * @param array $aplicante
+     * @return bool
+     */
+    private function evaluar(array $aplicante)
+    {
+        return $aplicante->nivel >= 20;
+    }
+}
---
+++ resources/views/prueba.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Prueba tecnica</title>
+
+</head>
+
+<body class="invisible">
+    <h1>Nombre: {{ $aplicante['nombre'] }}</h1>
+    @if ($aplicante->aprobado)
+        <h2>APROBADO</h1>
+        @else
+            <h2>REPROBADO</h1>
+    @endif
+</body>
+
+</html>
+<style>
+    .invisible {
+        display: none;
+    }
+</style>
